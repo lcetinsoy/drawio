b3bddedf2ac15639b9e6d8fd1a26c3ed34115c3e
Laurent Cetinsoy <laurent.cetinsoy@nowledgeable.com>
2023-05-14 20:20:10 +0200

    send both image and graph

--- src/main/webapp/plugins/parentIframeIO.js
+++ src/main/webapp/plugins/parentIframeIO.js
@@ -1,35 +1,55 @@
-Draw.loadPlugin(function(ui) {
-    // Fonction pour charger un graphe à partir d'une chaîne XML
-    function loadGraphFromXml(graph, xmlString) {
-      const doc = mxUtils.parseXml(xmlString);
-      const codec = new mxCodec(doc);
-      codec.decode(doc.documentElement, graph.getModel());
-    }
+Draw.loadPlugin(function (ui) {
+  // Fonction pour charger un graphe à partir d'une chaîne XML
+  function loadGraphFromXml(graph, xmlString) {
+    const doc = mxUtils.parseXml(xmlString);
+    const codec = new mxCodec(doc);
+    codec.decode(doc.documentElement, graph.getModel());
+  }
 
-    const parentWindow = window.parent;
-    parentWindow.postMessage({ type: 'pluginLoaded' }, '*');
-    
-    // Écoutez le message de la fenêtre parente pour charger le graphe
-    window.addEventListener('message', function(event) {
-      if (event.data.type === 'loadGraph') {
-        loadGraphFromXml(graph, event.data.graphXml);
-      }
-    });
-  
-    // Fonction pour obtenir le graphe au format XML
-    function getGraphXml(graph) {
-      const encoder = new mxCodec();
-      const node = encoder.encode(graph.getModel());
-      return mxUtils.getXml(node);
+
+
+  const parentWindow = window.parent;
+  parentWindow.postMessage({ type: 'pluginLoaded' }, '*');
+
+  // Écoutez le message de la fenêtre parente pour charger le graphe
+  window.addEventListener('message', function (event) {
+    if (event.data.type === 'loadGraph') {
+      loadGraphFromXml(graph, event.data.graphXml);
     }
-  
-    // Chargez le graphe à partir de la variable previousAnswer du parent
-    const graph = ui.editor.graph;
-    
-  
-    // Ajoutez un écouteur pour envoyer le graphe modifié au parent de l'iframe après chaque modification
-    graph.getModel().addListener(mxEvent.CHANGE, function() {
-      const graphXml = getGraphXml(graph);
-      parentWindow.postMessage({ type: 'graphChanged', graphXml: graphXml }, '*');
-    });
   });
+
+  // Fonction pour obtenir le graphe au format XML
+  function getGraphXml(graph) {
+    const encoder = new mxCodec();
+    const node = encoder.encode(graph.getModel());
+    return mxUtils.getXml(node);
+  }
+
+  // Chargez le graphe à partir de la variable previousAnswer du parent
+  const graph = ui.editor.graph;
+
+
+
+  function sendDiagramUpdate(graphXml) {
+    editorUi.editor.exportToCanvas( function (canvas, svgRoot) {
+
+      var dataUrl = canvas.toDataURL('image.png')
+  
+      window.parent.postMessage({ //use to send message for cross domains
+        type: 'graphChanged',
+        imageDataUrl: dataUrl,
+        graphXml: graphXml
+
+      }, "*");
+    })
+  }
+
+
+  // Ajoutez un écouteur pour envoyer le graphe modifié au parent de l'iframe après chaque modification
+  graph.getModel().addListener(mxEvent.CHANGE, function () {
+    const graphXml = getGraphXml(graph);
+    //parentWindow.postMessage({ type: 'graphChanged', graphXml: graphXml }, '*');
+  
+    sendDiagramUpdate(graphXml)
+  });
+});
